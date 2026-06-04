c1c7664f2ad370aca5b8d2f046fc11751755fa0e
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-06-04 18:30:53 +0300

    feat: the professor class that extends from academic class is defined -constructor defined -overrride methods defined

---
+++ UniversityManagementSystem/Professor.java
@@ -0,0 +1,25 @@
+//this class came from(extends) academicStaff class
+
+public class Professor extends AcademicStaff {
+    private int publishedPapers; // it is special to Professor
+
+    // Constructor
+    public Professor(int id, String name, String title, double baseSalary, int publishedPapers) {
+        super(id, name, title, baseSalary);
+        this.publishedPapers = publishedPapers;
+    }
+
+
+
+    //Polimorfizm (i override here)---> Formul: baseSalary + (publishedPapers * 1000)
+    
+    @Override
+    public double calculateSalary() {
+        return baseSalary + (publishedPapers * 1000);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Professor: " + getId() + " | " + getName() + " | " + title + " | Salary: " + calculateSalary());
+    }
+}
